3b6e42c9fce630df454b840f7bb9043fcec4ae44
riad <riadhossain@my.uopeople.edu>
2025-07-17 11:35:29 +0600

    Initial commit: DSA project with binary search tree implementation

---
+++ src/datastructures/binarysearchtree/Main.java
@@ -0,0 +1,19 @@
+package datastructures.binarysearchtree;
+public class Main {
+    public static void main(String[] args) {
+        BinarySearchTree myBST = new BinarySearchTree();
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+        myBST.insert(90);
+
+        myBST.insert(27);
+        System.out.println(myBST.root.right.right.right.value);
+        System.out.println(myBST.conatains(20));
+    }
+    
+    
+}
