f7df5724bdcee8666300b06cf8b6137d54ec0e57
riad <riadhossain@my.uopeople.edu>
2025-07-20 20:32:27 +0600

    Create rInsert & rContains method

--- src/datastructures/binarysearchtree/BinarySearchTree.java
+++ src/datastructures/binarysearchtree/BinarySearchTree.java
@@ -11,7 +11,22 @@ public class BinarySearchTree {
             left=right=null;
         }                                                                        
     }
+    //Recursive Insert
+    private Node rInsert(Node currentNode,int value){
+        if(currentNode == null) return new Node(value);
+        if(value < currentNode.value){
+            currentNode.left = rInsert(currentNode.left, value);
+        }else if(value > currentNode.value){
+            currentNode.right = rInsert(currentNode.right, value);
+        }
+        return currentNode;
+    }
+    public void rInsert(int value){
+        if(root==null) root = new Node(value);
+        rInsert(root,value);
+    }
 
+    //Non-Recursive
     public boolean insert(int value){
         Node newNode = new Node(value);
         if(root==null){
@@ -37,7 +52,22 @@ public class BinarySearchTree {
             
         }
     }
-
+    
+    //Rucursive Contains
+    private boolean rContains(Node currentNode, int value){
+        if(currentNode == null) return false;
+        if(currentNode.value == value) return true;
+        if(value<currentNode.value){
+            return rContains(currentNode.left, value);
+        }
+        else{
+            return rContains(currentNode.right, value);
+        }
+    }
+    public boolean rContains(int value){
+        return rContains(root,value);
+    }
+    //Non-Recursive 
     public boolean conatains(int value){
         if(root==null) return false;
         Node temp = root;
--- src/datastructures/binarysearchtree/Main.java
+++ src/datastructures/binarysearchtree/Main.java
@@ -2,18 +2,20 @@ package datastructures.binarysearchtree;
 public class Main {
     public static void main(String[] args) {
         BinarySearchTree myBST = new BinarySearchTree();
-        myBST.insert(47);
-        myBST.insert(21);
-        myBST.insert(76);
-        myBST.insert(18);
-        myBST.insert(52);
-        myBST.insert(82);
-        myBST.insert(90);
+        // myBST.insert(47);
+        // myBST.insert(21);
+        // myBST.insert(76);
+        // myBST.insert(18);
+        // myBST.insert(52);
+        // myBST.insert(82);
+        // myBST.insert(90);
 
-        myBST.insert(27);
-        myBST.insert(100);
-        System.out.println(myBST.root.right.right.right.value);
-        System.out.println(myBST.conatains(20));
+        myBST.rInsert(27);
+        myBST.rInsert(100);
+        System.out.println(myBST.root.right.value);
+        System.out.println(myBST.rContains(20));
+        System.out.println("contain => 100");
+        System.out.println(myBST.rContains(100));
     }
     
      
